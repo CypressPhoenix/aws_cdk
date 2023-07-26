28771a5dac818350e3ffe862a9eab099b9a73b62
Kryvobok <kryvobok@gmail.com>
2023-07-27 00:25:29 +0300

    update infra front

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from aws_cdk import App
 from ovrs_code_pipeline.forntmain import FrontMain
 from ovrs_code_pipeline.frontdev import FrontDev
+from front_infrastructure.front_infra_main import FrontInfrastructure
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -8,6 +9,7 @@ load_dotenv()
 region = os.environ.get("REGION")
 
 app = App()
+FrontInfrastructure(app, "FrontInfra", env={'region': region})
 FrontDev(app, "FrontDevStack", env={'region': region})
 FrontMain(app, "FrontMainStack", env={'region': region})
 app.synth()
--- ovrs_code_pipeline/forntmain.py
+++ ovrs_code_pipeline/forntmain.py
@@ -3,10 +3,13 @@ from aws_cdk import (
     aws_codepipeline_actions as codepipeline_actions,
     aws_codebuild as codebuild,
     Stack,
+    RemovalPolicy,
 )
+import aws_cdk.aws_s3 as s3
 from constructs import Construct
 import os
 from dotenv import load_dotenv
+from aws_cdk.aws_codebuild import LinuxBuildImage
 
 load_dotenv()
 class FrontMain(Stack):
@@ -18,8 +21,14 @@ class FrontMain(Stack):
         git_repo_name = os.environ.get("GIT_REPO_NAME_FRONT_OVRS")
         git_repo_owner = os.environ.get("GIT_REPO_OWNER_OVRS")
 
+        bucket = s3.Bucket(
+            self,
+            "BucketForFrontEndMain",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
 
-        pipeline_1 = codepipeline.Pipeline(self, "Pipeline1", pipeline_name="Pipeline1")
+        FrontMainPipeline = codepipeline.Pipeline(self, "FrontMain", pipeline_name="FrontMain", artifact_bucket=bucket)
 
         source_output = codepipeline.Artifact()
         build_output = codepipeline.Artifact()
@@ -34,14 +43,14 @@ class FrontMain(Stack):
             trigger_on_push=True,
         )
 
-        pipeline_1.add_stage(stage_name="Source", actions=[github_source_action])
+        FrontMainPipeline.add_stage(stage_name="Source", actions=[github_source_action])
 
         project = codebuild.PipelineProject(
             self,
             "Pipeline1Project",
             build_spec=codebuild.BuildSpec.from_source_filename("buildspec.yml"),
             environment=codebuild.BuildEnvironment(
-                build_image=codebuild.LinuxBuildImage.STANDARD_1_0
+                build_image=LinuxBuildImage.from_code_build_image_id("aws/codebuild/standard:4.0")
             ),
         )
 
@@ -53,4 +62,4 @@ class FrontMain(Stack):
         )
 
 
-        pipeline_1.add_stage(stage_name="Build", actions=[build_action])
+        FrontMainPipeline.add_stage(stage_name="Build", actions=[build_action])
